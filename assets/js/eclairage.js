bcb9264de3de5ff30b7105efb94182db5ec8366f
JeremieLarousse <97170000+JeremieLarousse@users.noreply.github.com>
2022-02-25 15:38:16 +0100

    add

---
+++ assets/js/eclairage.js
@@ -0,0 +1,29 @@
+let boite = document.getElementById("contenu");
+boite.style.textAlign= "center";
+
+for (i = 0; i < material.length; i++){
+    
+    let leNom = document.createElement("p");
+    leNom.classList.add("NomMatos");
+    leNom.textContent = material[i].nom;
+    boite.appendChild(leNom);
+
+    let newDiv = document.createElement("div");
+    boite.appendChild(newDiv);
+
+    let photo = document.createElement("img");
+    photo.classList.add("imageMatos");
+    photo.src = material[i].image;
+    newDiv.appendChild(photo);
+
+    let laReference = document.createElement("p");
+    laReference .classList.add("ReferenceMatos");
+    laReference .textContent = material[i].reference;
+    boite.appendChild(laReference);
+
+    let lePrix = document.createElement("p");
+    lePrix .classList.add("PrixMatos");
+    lePrix .textContent = material[i].prix;
+    boite.appendChild(lePrix);
+
+}
