5da974fd5c47f32c940539b3804f249b84d34542
JeremieLarousse <97170000+JeremieLarousse@users.noreply.github.com>
2022-03-04 13:56:25 +0100

    add

--- assets/js/eclairage.js
+++ assets/js/eclairage.js
@@ -1,4 +1,5 @@
 let boite = document.getElementById("contenu");
+boite.classList.add("laBoite");
 boite.style.textAlign= "center";
 
 for (i = 0; i < material.length; i++){
@@ -12,18 +13,28 @@ for (i = 0; i < material.length; i++){
     boite.appendChild(newDiv);
 
     let photo = document.createElement("img");
+    // photo.style.width = "300px";
     photo.classList.add("imageMatos");
     photo.src = material[i].image;
     newDiv.appendChild(photo);
 
     let laReference = document.createElement("p");
-    laReference .classList.add("ReferenceMatos");
-    laReference .textContent = material[i].reference;
+    laReference.classList.add("ReferenceMatos");
+    laReference.textContent = material[i].reference;
     boite.appendChild(laReference);
 
     let lePrix = document.createElement("p");
-    lePrix .classList.add("PrixMatos");
-    lePrix .textContent = material[i].prix;
+    lePrix.classList.add("PrixMatos");
+    lePrix.textContent = material[i].prix;
     boite.appendChild(lePrix);
+
+    let boutonAchat = document.createElement("button");
+    boutonAchat.classList.add("boutonAchat");
+    boutonAchat.innerHTML = "AJOUTER";
+    boite.appendChild(boutonAchat);
+
+    let barre = document.createElement("p")
+    barre.classList.add("barreSeparation");
+    boite.appendChild(barre);
 
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,11 @@ class electro{
     constructor(w, x, y, z){
         this.marque = w;
         this.reference = x;
-        this.couleur = y;
+        this.prix = y;
         this.image = z;
     }
 
-    affichemarque(w){
+    affichemarque(x){
         return x + "De la marque : " + this.marque;
     }
 
@@ -16,43 +16,43 @@ class electro{
         return x + "Voici : " + this.reference;
     }
 
-    affichecouleur(y){
-        return x + "Sa couleur est : " + this.couleur;
+    afficheprix(x){
+        return x + "Son prix est : " + this.prix;
     }
 
-    afficheimage(z){
+    afficheimage(x){
         return x + "Et enfin; voici sa photo : " + this.image;
     }
 }
 
-const produit = new electro("", "", "", "");
+const produit = new electro("PLUGGER STUDIO", "RING LITE10 SET", "29€", "");
 document.getElementById("stat1").innerHTML = produit.affichemarque("");
 document.getElementById("stat2").innerHTML += produit.affichereference("");
-document.getElementById("stat3").innerHTML += produit.affichecouleur("");
+document.getElementById("stat3").innerHTML += produit.afficheprix("");
 document.getElementById("stat4").innerHTML += produit.afficheimage("");
 
-const produit1 = new electro("", "", "", "");
+const produit1 = new electro("Gobikey", "Boule Disco de Commande Vocale,7 Couleurs", "15.99€", "");
 document.getElementById("stat5").innerHTML += produit1.affichemarque("");
 document.getElementById("stat6").innerHTML += produit1.affichereference("");
-document.getElementById("stat7").innerHTML += produit1.affichecouleur("");
+document.getElementById("stat7").innerHTML += produit1.afficheprix("");
 document.getElementById("stat8").innerHTML += produit1.afficheimage("");
 
-const produit2 = new electro("", "", "", "");
+const produit2 = new electro("Sony", "Sony MHC-V02", "230€", "");
 document.getElementById("stat9").innerHTML = produit2.affichemarque("");
 document.getElementById("stat10").innerHTML += produit2.affichereference("");
-document.getElementById("stat11").innerHTML += produit2.affichecouleur("");
+document.getElementById("stat11").innerHTML += produit2.afficheprix("");
 document.getElementById("stat12").innerHTML += produit2.afficheimage("");
 
-const produit3 = new electro("", "", "", "");
+const produit3 = new electro("Ibiza Sound", "SPLBOX350-PORT - Ibiza Sound", "179.90€", "");
 document.getElementById("stat13").innerHTML = produit3.affichemarque("");
 document.getElementById("stat14").innerHTML += produit3.affichereference("");
-document.getElementById("stat15").innerHTML += produit3.affichecouleur("");
+document.getElementById("stat15").innerHTML += produit3.afficheprix("");
 document.getElementById("stat16").innerHTML += produit3.afficheimage("");
 
-const produit4 = new electro("", "", "", "");
+const produit4 = new electro("LEICKE", "LEICKE Enceinte Sono", "129.99€", "");
 document.getElementById("stat17").innerHTML += produit4.affichemarque("");
 document.getElementById("stat18").innerHTML += produit4.affichereference("");
-document.getElementById("stat19").innerHTML += produit4.affichecouleur("");
+document.getElementById("stat19").innerHTML += produit4.afficheprix("");
 document.getElementById("stat20").innerHTML += produit4.afficheimage("");
 
 /////////////////////////BOUTON slider///////////////////////
